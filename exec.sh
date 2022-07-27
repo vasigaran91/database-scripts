54cb63ad67119924cde51e2939aa70ef41998b09
vasigaran91 <44626398+vasigaran91@users.noreply.github.com>
2022-07-27 16:19:34 +0300

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/vasigaran91/database-scripts/init-custom.php -O /root/mailwizz-docker/mailwizz/html/apps/init-custom.php
+wget https://raw.githubusercontent.com/vasigaran91/database-scripts/init-custom.php -O /apps/customer/views/campaign_reports/helper.php
